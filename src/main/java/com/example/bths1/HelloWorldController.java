14f118b0e4e6ecb2ddafdc095361d4346f4fb1f9
trong373 <dotrong403@gmail.com>
2024-11-25 18:03:38 +0700

    first commit

--- src/main/java/com/example/bths1/HelloWorldController.java
+++ src/main/java/com/example/bths1/HelloWorldController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController {
 
-    @GetMapping("/hello") // Đảm bảo đường dẫn đúng.
+    @GetMapping("/hello") 
     public String sayHello() {
         return "Hello World!";
     }
